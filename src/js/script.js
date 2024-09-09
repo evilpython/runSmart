11fb9fda173eb4f5a2433726e950611ad50bf755
evilpython <a.rebeka@protonmail.com>
2024-09-09 15:58:04 +0700

    Create modals+adaptive

--- src/js/script.js
+++ src/js/script.js
@@ -1,58 +1,74 @@
 $(document).ready(function () {
-    $('.carousel__inner').slick({
-        prevArrow:
-            '<button type="button" class="slick-prev"><img src="../ico/left.svg"/></button>',
-        nextArrow:
-            '<button type="button" class="slick-next"><img src="../ico/right.svg"/></button>',
-        dots: false,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        responsive: [
-            {
-                breakpoint: 900,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    infinite: true,
-                    arrows: false,
-                    dots: true,
-                },
-            },
-        ],
-    });
+  $(".carousel__inner").slick({
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="../ico/left.svg"/></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="../ico/right.svg"/></button>',
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          arrows: false,
+          dots: true,
+        },
+      },
+    ],
+  });
 
-    $('ul.catalog__tabs').on(
-        'click',
-        'li:not(.catalog__tab_active)',
-        function () {
-            $(this)
-                .addClass('catalog__tab_active')
-                .siblings()
-                .removeClass('catalog__tab_active')
-                .closest('div.container')
-                .find('div.catalog__content')
-                .removeClass('catalog__content_active')
-                .eq($(this).index())
-                .addClass('catalog__content_active');
-        }
-    );
-
-    function toggleSlide(item) {
-        $(item).each(function (i) {
-            $(this).on('click', function (e) {
-                e.preventDefault();
-                $('.catalog-item__content')
-                    .eq(i)
-                    .toggleClass('catalog-item__content_active');
-                $('.catalog-item__specifications')
-                    .eq(i)
-                    .toggleClass('catalog-item__specifications_active');
-            });
-        });
+  $("ul.catalog__tabs").on(
+    "click",
+    "li:not(.catalog__tab_active)",
+    function () {
+      $(this)
+        .addClass("catalog__tab_active")
+        .siblings()
+        .removeClass("catalog__tab_active")
+        .closest("div.container")
+        .find("div.catalog__content")
+        .removeClass("catalog__content_active")
+        .eq($(this).index())
+        .addClass("catalog__content_active");
     }
+  );
 
-    toggleSlide('.catalog-item__link');
-    toggleSlide('.catalog-item__back');
+  function toggleSlide(item) {
+    $(item).each(function (i) {
+      $(this).on("click", function (e) {
+        e.preventDefault();
+        $(".catalog-item__content")
+          .eq(i)
+          .toggleClass("catalog-item__content_active");
+        $(".catalog-item__specifications")
+          .eq(i)
+          .toggleClass("catalog-item__specifications_active");
+      });
+    });
+  }
+
+  toggleSlide(".catalog-item__link");
+  toggleSlide(".catalog-item__back");
+
+  // Modal
+  $("[data-modal=consultation]").on("click", function () {
+    $(".overlay, #consultation").fadeIn();
+  });
+
+  $(".modal__close").on("click", function () {
+    $(".overlay, #consultation, #order, #thanks").fadeOut();
+  });
+
+  $(".button_mini").each(function (i) {
+    $(this).on("click", function () {
+      $("#order .modal__descr").text($(".catalog-item__subtitle").eq(i).text());
+      $(".overlay, #order").fadeIn();
+    });
+  });
 });
